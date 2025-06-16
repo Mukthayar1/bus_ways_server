e8a0b788f9d8c8cac9ab19addf6c50977c0de4dc
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2025-06-16 14:42:08 +0500

    updated

--- app.js
+++ app.js
@@ -1,54 +1,3 @@
-// import dontenv from "dotenv";
-// import express from "express";
-// import cors from "cors";
-
-// import connectDB from "./config/connect.js";
-// import { PORT } from "./config/config.js";
-
-// import userRoutes from "./routes/user.js";
-// import busRoutes from "./routes/bus.js";
-// import ticketRoutes from './routes/ticket.js'
-// import { buildAdminJS } from "./config/setup.js";
-
-
-// dontenv.config();
-// const app = express();
-// const corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "POST", "POST", "DELETE", "PUT", "MATCH"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.send("Welcome to Bus Ways API");
-// }
-// );
-// app.use("/user", userRoutes);
-// app.use("/bus", busRoutes);
-// app.use("/ticket", ticketRoutes);
-// app.use(admin.options.rootPath, adminRouter);
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URL);
-//     app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
-//       if (err) {
-//         console.log("Failed to listen server", err);
-//       } else {
-//         console.log(
-//           `server started successfully started: https//:localhost:${PORT}`
-//         );
-//       }
-//     });
-//   } catch (error) {
-//     console.log("Failed to start server", error);
-//   }
-// };
-
-// start();
-
-
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -86,7 +35,7 @@ const start = async () => {
     await connectDB(process.env.MONGO_URL);
 
     // ✅ Initialize AdminJS
-    await buildAdminJS(app);
+    // await buildAdminJS(app);
 
     // ✅ Start the server
     app.listen(PORT, "0.0.0.0", (err) => {
